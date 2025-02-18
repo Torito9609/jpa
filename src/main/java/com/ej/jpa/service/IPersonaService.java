ee730083e45e2eaa27cac9ef1f81a8fb57c55f2b
Torito9609 <toroandres85@gmail.com>
2025-02-18 09:48:37 -0500

    actualizacion del codigo 09:48

--- src/main/java/com/ej/jpa/service/IPersonaService.java
+++ src/main/java/com/ej/jpa/service/IPersonaService.java
@@ -1,4 +1,18 @@
 package com.ej.jpa.service;
+import com.ej.jpa.model.Persona;
+import java.util.List;
 
 public interface IPersonaService {
+    //método de traer personas Read o lectura
+    public List<Persona> getPersonas();
+
+    //método para guardar personas
+    public void savePersona(Persona persona);
+    
+    //método para borrar una persona (eliminar por id)
+    public void deletePersona(Long id_persona);
+
+    //método para buscar personas
+    public Persona findPersona(Long id_persona);
+
 }
--- src/main/java/com/ej/jpa/service/PersonaService.java
+++ src/main/java/com/ej/jpa/service/PersonaService.java
@@ -1,7 +1,36 @@
 package com.ej.jpa.service;
 
+import com.ej.jpa.model.Persona;
+import com.ej.jpa.repository.IPersonaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class PersonaService {
+public class PersonaService implements IPersonaService{
+
+    @Autowired
+    private IPersonaRepository personaRepository;
+
+    @Override
+    public List<Persona> getPersonas() {
+        //.findAll retorna una lista de personas
+        return personaRepository.findAll();
+    }
+
+    @Override
+    public void savePersona(Persona persona) {
+        personaRepository.save(persona);
+    }
+
+    @Override
+    public void deletePersona(Long id_persona) {
+        personaRepository.deleteById(id_persona);
+    }
+
+    @Override
+    public Persona findPersona(Long id_persona) {
+        return null;
+    }
 }
